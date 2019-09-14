0f62da0765df3ef69538e40819dc6699d0bde633
Joshua Villing <jsvilling@gmail.com>
2019-09-14 10:30:48 +0200

    Add Command and DomainEntity interface

---
+++ src/main/kotlin/ch/jvi/budgetmanager/backend/domain/Command.kt
@@ -0,0 +1,8 @@
+package ch.jvi.budgetmanager.backend.domain
+
+/**
+ * Marker interface for all commands
+ *
+ * @author J. Villing
+ */
+interface Command
---
+++ src/main/kotlin/ch/jvi/budgetmanager/backend/domain/DomainEntity.kt
@@ -0,0 +1,8 @@
+package ch.jvi.budgetmanager.backend.domain
+
+/**
+ * Marker interface for all domain entities
+ *
+ * @author J. Villing
+ */
+interface DomainEntity
