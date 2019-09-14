368ebacbd4c64b1d3d0e26f0f130318daff2c1b1
Joshua Villing <jsvilling@gmail.com>
2019-09-14 13:46:34 +0200

    Save created command to commandstore in AccountMessageListener

--- src/main/kotlin/ch/jvi/budgetmanager/backend/core/message/AccountMessageListener.kt
+++ src/main/kotlin/ch/jvi/budgetmanager/backend/core/message/AccountMessageListener.kt
@@ -2,16 +2,18 @@ package ch.jvi.budgetmanager.backend.core.message
 
 import ch.jvi.budgetmanager.backend.api.command.Command
 import ch.jvi.budgetmanager.backend.api.command.CommandBus
+import ch.jvi.budgetmanager.backend.api.command.CommandStore
 import ch.jvi.budgetmanager.backend.domain.account.AccountCommand
 import ch.jvi.budgetmanager.core.api.MessageListener
 import org.springframework.stereotype.Component
 
 @Component
-class AccountMessageListener(private val commandBus: CommandBus) {
+class AccountMessageListener(private val commandStore: CommandStore, private val commandBus: CommandBus) {
 
     @MessageListener
     fun handle(createAccountMessage: CreateAccountMessage) {
         val createAccountCommand = convertToCreationCommand(createAccountMessage)
+        commandStore.save(createAccountCommand)
         commandBus.send(createAccountCommand)
     }
 
---
+++ src/test/kotlin/ch/jvi/budgetmanager/backend/core/message/AccountMessageListenerTest.kt
@@ -0,0 +1,31 @@
+package ch.jvi.budgetmanager.backend.core.message
+
+import ch.jvi.budgetmanager.backend.api.command.CommandBus
+import ch.jvi.budgetmanager.backend.api.command.CommandStore
+import ch.jvi.budgetmanager.backend.domain.account.AccountCommand
+import org.junit.Test
+import org.mockito.Mockito
+import java.math.BigDecimal
+
+internal class AccountMessageListenerTest {
+
+    private val commandStore = Mockito.mock(CommandStore::class.java)
+    private val commandBus = Mockito.mock(CommandBus::class.java)
+    private val accountMessageListener = AccountMessageListener(commandStore, commandBus)
+
+    @Test
+    fun testHandleCreateAccountMessage() {
+        // Given
+        val balance = BigDecimal.TEN
+        val name = "NameName"
+        val receivedCreationMessage = CreateAccountMessage(balance, name)
+        val expectedCreationCommand = AccountCommand.CreateAccountCommand(balance, name)
+
+        // When
+        accountMessageListener.handle(receivedCreationMessage)
+
+        // Then
+        Mockito.verify(commandStore, Mockito.times(1)).save(expectedCreationCommand)
+        Mockito.verify(commandBus, Mockito.times(1)).send(expectedCreationCommand)
+    }
+}
