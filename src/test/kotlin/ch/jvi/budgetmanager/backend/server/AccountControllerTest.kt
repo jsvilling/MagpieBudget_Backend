40c97611686ce62da06a763cdc16158284e0aae6
Joshua Villing <jsvilling@gmail.com>
2019-09-14 19:05:38 +0200

    Add getAccount to AccountController

--- src/test/kotlin/ch/jvi/budgetmanager/backend/server/AccountControllerTest.kt
+++ src/test/kotlin/ch/jvi/budgetmanager/backend/server/AccountControllerTest.kt
@@ -3,25 +3,38 @@ package ch.jvi.budgetmanager.backend.server
 import ch.jvi.budgetmanager.backend.core.AccountService
 import ch.jvi.budgetmanager.core.api.MessageBus
 import org.junit.Test
-import org.mockito.Mockito
+import org.mockito.Mockito.*
 import java.math.BigDecimal
+import org.mockito.Mockito as Mockito
 
 internal class AccountControllerTest {
 
-    var messageBus = Mockito.mock(MessageBus::class.java)
-    var accountService = Mockito.spy(AccountService(messageBus))
+    var messageBus = mock(MessageBus::class.java)
+    var accountService = spy(AccountService(messageBus))
     var accountController = AccountController(accountService)
 
     @Test
-    fun testCreateAccountCall() {
+    fun testGetAccount() {
+        // Given
+        val id = "someId"
+
         // When
+        accountController.getAccount(id)
+
+        // Then
+        verify(accountService, times(1)).getAccount(id)
+    }
+
+    @Test
+    fun testCreateAccount() {
+        // Given
         val balance = BigDecimal.ONE
         val name = "Name";
+
+        // When
         accountController.createAccount(balance, name)
 
         // Then
-        Mockito.verify(accountService, Mockito.times(1)).createAccount(balance, name)
+        verify(accountService, times(1)).createAccount(balance, name)
     }
-
-
 }
