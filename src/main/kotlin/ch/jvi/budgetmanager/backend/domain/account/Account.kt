5697eb291ab96e9ddacdc468a538c94e0efc8e3e
Joshua Villing <jsvilling@gmail.com>
2019-09-19 08:45:38 +0200

    Use AccountCommand for apply method in Account

--- src/main/kotlin/ch/jvi/budgetmanager/backend/domain/account/Account.kt
+++ src/main/kotlin/ch/jvi/budgetmanager/backend/domain/account/Account.kt
@@ -1,12 +1,13 @@
 package ch.jvi.budgetmanager.backend.domain.account
 
 import ch.jvi.budgetmanager.backend.domain.DomainEntity
+import ch.jvi.budgetmanager.backend.domain.account.AccountCommand.CreateAccountCommand
+import ch.jvi.budgetmanager.backend.domain.account.AccountCommand.UpdateAccountCommand
 import java.math.BigDecimal
-import ch.jvi.budgetmanager.backend.domain.account.AccountCommand.*
 
 class Account(creationCommand: CreateAccountCommand) : DomainEntity {
 
-    val id: String = creationCommand.id;
+    val id: String = creationCommand.id
 
     var balance: BigDecimal = creationCommand.balance
         private set
@@ -14,9 +15,17 @@ class Account(creationCommand: CreateAccountCommand) : DomainEntity {
     var name: String = creationCommand.name
         private set
 
-    fun apply(command: UpdateAccountCommand) {
+    fun apply(command: AccountCommand) = when (command) {
+        is CreateAccountCommand -> apply(command)
+        is UpdateAccountCommand -> apply(command)
+    }
+
+    private fun apply(command: CreateAccountCommand): Nothing {
+        throw IllegalArgumentException("Creation commands cannot be applied to existing account")
+    }
+
+    private fun apply(command: UpdateAccountCommand) {
         this.balance = command.balance
         this.name = command.name
     }
-
 }
