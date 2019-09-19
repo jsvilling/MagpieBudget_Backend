2fb490804f40449b6fd695b6492a4edb954f75e0
Joshua Villing <jsvilling@gmail.com>
2019-09-19 08:15:39 +0200

    Add apply function for updating account

--- src/main/kotlin/ch/jvi/budgetmanager/backend/domain/account/Account.kt
+++ src/main/kotlin/ch/jvi/budgetmanager/backend/domain/account/Account.kt
@@ -1,17 +1,21 @@
 package ch.jvi.budgetmanager.backend.domain.account
 
-import ch.jvi.budgetmanager.backend.api.command.Command
 import ch.jvi.budgetmanager.backend.domain.DomainEntity
 import java.math.BigDecimal
 
 class Account(creationCommand: CreateAccountCommand) : DomainEntity {
 
     val id: String = creationCommand.id;
-    val balance: BigDecimal = creationCommand.balance;
-    val name: String = creationCommand.name
 
-    fun apply(commands: List<Command>) {
+    var balance: BigDecimal = creationCommand.balance
+        private set
 
+    var name: String = creationCommand.name
+        private set
+
+    fun apply(command: UpdateAccountCommand) {
+        this.balance = command.balance
+        this.name = command.name
     }
 
 }
