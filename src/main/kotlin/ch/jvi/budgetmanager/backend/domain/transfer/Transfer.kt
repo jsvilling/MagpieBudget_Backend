45719ba17de50b9c7f2f440877fa194db1cf1bd6
Joshua Villing <jsvilling@gmail.com>
2019-09-20 08:31:23 +0200

    Add value fields to Transaction
    recipient, sender and amount

--- src/main/kotlin/ch/jvi/budgetmanager/backend/domain/transfer/Transfer.kt
+++ src/main/kotlin/ch/jvi/budgetmanager/backend/domain/transfer/Transfer.kt
@@ -6,6 +6,9 @@ import ch.jvi.budgetmanager.backend.domain.transfer.TransferCommand.*
 class Transfer(creationCommand: CreateTransferCommand) : DomainEntity<TransferCommand> {
 
     val id = creationCommand.id
+    val recipientId = creationCommand.recipientId
+    val senderId = creationCommand.senderId
+    val amount = creationCommand.amount
 
     override fun apply(command: TransferCommand) = when(command) {
         is CreateTransferCommand -> apply(command)
--- src/main/kotlin/ch/jvi/budgetmanager/backend/domain/transfer/TransferCommand.kt
+++ src/main/kotlin/ch/jvi/budgetmanager/backend/domain/transfer/TransferCommand.kt
@@ -3,9 +3,13 @@ package ch.jvi.budgetmanager.backend.domain.transfer
 import ch.jvi.budgetmanager.backend.api.command.Command
 import ch.jvi.budgetmanager.backend.api.command.CreationCommand
 import ch.jvi.budgetmanager.backend.domain.IDProvider
+import java.math.BigDecimal
 
 sealed class TransferCommand : Command {
     data class CreateTransferCommand(
+        val recipientId: String,
+        val senderId: String,
+        val amount: BigDecimal,
         override val id: String = IDProvider.next()
     ) : TransferCommand(), CreationCommand
 }
