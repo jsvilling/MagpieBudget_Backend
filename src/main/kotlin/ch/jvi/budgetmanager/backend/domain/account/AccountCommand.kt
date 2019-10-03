b4e105d92d12a6030848dbe1ddcc1682d0a8c993
Joshua Villing <jsvilling@gmail.com>
2019-10-03 09:25:10 +0200

    Add minmal mongodb configuration

--- src/main/kotlin/ch/jvi/budgetmanager/backend/domain/account/AccountCommand.kt
+++ src/main/kotlin/ch/jvi/budgetmanager/backend/domain/account/AccountCommand.kt
@@ -3,22 +3,27 @@ package ch.jvi.budgetmanager.backend.domain.account
 import ch.jvi.budgetmanager.backend.api.command.Command
 import ch.jvi.budgetmanager.backend.api.command.CreationCommand
 import ch.jvi.budgetmanager.backend.domain.IDProvider
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
 import java.math.BigDecimal
 
-sealed class AccountCommand(override val commandId: String = IDProvider.next()) : Command {
+sealed class AccountCommand(@Id override val commandId: String = IDProvider.next()) : Command {
 
+    @Document(collection = "CreateAccountCommand")
     data class CreateAccountCommand(
         val balance: BigDecimal = BigDecimal.ZERO,
         val name: String = "Anonymous Account",
         override val entityId: String = IDProvider.next()
     ) : AccountCommand(), CreationCommand
 
+    @Document(collection = "UpdateAccountCommand")
     data class UpdateAccountCommand(
         val balance: BigDecimal = BigDecimal.ZERO,
         val name: String = "",
         override val entityId: String
     ) : AccountCommand()
 
+    @Document(collection = "AdjustAccountBalanceCommand")
     data class AdjustAccountBalanceCommand(
         val balanceChange: BigDecimal,
         override val entityId: String,
