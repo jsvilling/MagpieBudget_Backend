d170de207f979917cbbc274f05f96f7eb41f15b3
Joshua Villing <joshua.villing@students.fhnw.ch>
2020-03-29 11:37:40 +0200

    Implement findAll for transfers

--- src/main/kotlin/ch/jvi/budgetmanager/backend/core/service/TransferService.kt
+++ src/main/kotlin/ch/jvi/budgetmanager/backend/core/service/TransferService.kt
@@ -1,11 +1,13 @@
 package ch.jvi.budgetmanager.backend.core.service
 
+import ch.jvi.budgetmanager.backend.api.command.CreationCommand
 import ch.jvi.budgetmanager.backend.api.command.store.CommandStore
 import ch.jvi.budgetmanager.backend.api.event.EventBus
 import ch.jvi.budgetmanager.backend.api.service.EntityService
 import ch.jvi.budgetmanager.backend.core.event.TransferEvent.CreateTransferEvent
 import ch.jvi.budgetmanager.backend.core.event.TransferEvent.UpdateTransferEvent
 import ch.jvi.budgetmanager.backend.domain.transfer.Transfer
+import ch.jvi.budgetmanager.backend.domain.transfer.TransferCommand
 import ch.jvi.budgetmanager.backend.domain.transfer.TransferCommand.CreateTransferCommand
 import org.springframework.stereotype.Service
 import java.math.BigDecimal
@@ -23,8 +25,24 @@ class TransferService(
      */
     override fun find(entity: String): Transfer {
         val createTransferCommand = commandStore.findCreationCommand(entity) as CreateTransferCommand
-        val transferCommands = commandStore.findTransferCommands(entity)
-        return Transfer(createTransferCommand)
+        val transfer = Transfer(createTransferCommand)
+        return applyCommands(transfer)
+    }
+
+    override fun findAll(): List<Transfer> {
+        return commandStore.findCreationCommands(this::isTransferCreationCommand)
+            .map { Transfer(it as CreateTransferCommand) }
+            .map { applyCommands(it) }
+    }
+
+    private fun isTransferCreationCommand(command: CreationCommand): Boolean {
+        return command is CreateTransferCommand;
+    }
+
+    private fun applyCommands(transfer: Transfer): Transfer {
+        val commands: List<TransferCommand> = commandStore.findTransferCommands(transfer.id)
+        transfer.applyAll(commands)
+        return transfer
     }
 
     /**
@@ -42,10 +60,6 @@ class TransferService(
     fun updateTransfer(id: String, senderId: String, recipientId: String, amount: BigDecimal) {
         val updateTransferMessage = UpdateTransferEvent(id, recipientId, senderId, amount)
         eventBus.send(updateTransferMessage)
-    }
-
-    override fun findAll(): List<Transfer> {
-        TODO("Not yet implemented")
     }
 
 }
--- src/main/kotlin/ch/jvi/budgetmanager/backend/server/controller/TransferController.kt
+++ src/main/kotlin/ch/jvi/budgetmanager/backend/server/controller/TransferController.kt
@@ -1,6 +1,7 @@
 package ch.jvi.budgetmanager.backend.server.controller
 
 import ch.jvi.budgetmanager.backend.core.service.TransferService
+import ch.jvi.budgetmanager.backend.domain.transfer.Transfer
 import org.springframework.web.bind.annotation.*
 import java.math.BigDecimal
 
@@ -13,13 +14,23 @@ import java.math.BigDecimal
 @RequestMapping("/api/transfers")
 class TransferController(private val transferService: TransferService) {
 
+    @GetMapping()
+    fun get(): List<Transfer> {
+        return transferService.findAll()
+    }
+
     @GetMapping("/{id}")
     fun get(@PathVariable id: String) {
         transferService.find(id)
     }
 
     @PutMapping("/create")
-    fun create(@RequestParam senderId: String, @RequestParam recipientId: String, @RequestParam amount: BigDecimal, @RequestParam budgetId: String) {
+    fun create(
+        @RequestParam senderId: String,
+        @RequestParam recipientId: String,
+        @RequestParam amount: BigDecimal,
+        @RequestParam budgetId: String
+    ) {
         transferService.createTransfer(senderId, recipientId, amount, budgetId)
     }
 
