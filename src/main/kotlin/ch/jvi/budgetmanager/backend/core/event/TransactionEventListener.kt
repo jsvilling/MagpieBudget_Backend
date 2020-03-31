adf421d9365c7786bf0dcb590a057bb5fae8ef5a
Joshua Villing <joshua.villing@students.fhnw.ch>
2020-03-31 16:19:48 +0200

    Implement transaction event listener

---
+++ src/main/kotlin/ch/jvi/budgetmanager/backend/core/event/TransactionEventListener.kt
@@ -0,0 +1,25 @@
+package ch.jvi.budgetmanager.backend.core.event
+
+import ch.jvi.budgetmanager.backend.api.command.bus.CommandBus
+import ch.jvi.budgetmanager.backend.api.command.store.CommandStore
+import ch.jvi.budgetmanager.backend.core.event.TransactionEvent.CreateTransactionEvent
+import ch.jvi.budgetmanager.backend.domain.transaction.TransactionCommand.CreateTransactionCommand
+import ch.jvi.budgetmanager.core.api.EventListener
+import org.springframework.stereotype.Component
+
+@Component
+class TransactionEventListener(
+    private val commandBus: CommandBus,
+    private val commandStore: CommandStore
+) {
+    @EventListener
+    fun handle(createTransactionEvent: CreateTransactionEvent) {
+        val createTransactionCommand = CreateTransactionCommand(
+            name = createTransactionEvent.name,
+            amount = createTransactionEvent.amount,
+            type = createTransactionEvent.type
+        )
+        commandBus.send(createTransactionCommand)
+        commandStore.saveCreationCommand(createTransactionCommand)
+    }
+}
