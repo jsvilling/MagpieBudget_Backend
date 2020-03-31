30269d6fb70f96dac4b1c085de46983460023bbf
Joshua Villing <joshua.villing@students.fhnw.ch>
2020-03-31 15:48:59 +0200

    Setup transaction domain classes

---
+++ src/main/kotlin/ch/jvi/budgetmanager/backend/domain/transaction/Transaction.kt
@@ -0,0 +1,33 @@
+package ch.jvi.budgetmanager.backend.domain.transaction
+
+import ch.jvi.budgetmanager.backend.domain.DomainEntity
+import ch.jvi.budgetmanager.backend.domain.transaction.TransactionCommand.CreateTransactionCommand
+
+/**
+ * Domain Entity representing the transaction of an amount to or from an account.
+ *
+ * A transaction can be either income or an expense.
+ *
+ * @author J. Villing
+ */
+class Transaction(creationCommand: CreateTransactionCommand) : DomainEntity<TransactionCommand> {
+
+    val id = creationCommand.entityId
+
+    var name = creationCommand.name
+        private set
+
+    var amount = creationCommand.amount
+        private set
+
+    var type = creationCommand.type
+        private set
+
+    override fun apply(command: TransactionCommand) = when (command) {
+        is CreateTransactionCommand -> apply(command)
+    }
+
+    private fun apply(command: CreateTransactionCommand): Nothing =
+        throw IllegalArgumentException("Creation commands cannot be applied to existing account")
+
+}
---
+++ src/main/kotlin/ch/jvi/budgetmanager/backend/domain/transaction/TransactionCommand.kt
@@ -0,0 +1,21 @@
+package ch.jvi.budgetmanager.backend.domain.transaction
+
+import ch.jvi.budgetmanager.backend.api.command.Command
+import ch.jvi.budgetmanager.backend.api.command.CreationCommand
+import ch.jvi.budgetmanager.backend.domain.IDProvider
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
+import java.math.BigDecimal
+
+@Document(collection = "command")
+sealed class TransactionCommand : Command {
+
+    data class CreateTransactionCommand(
+        val name: String,
+        val amount: BigDecimal,
+        val type: TransactionType,
+        override val entityId: String = IDProvider.next(),
+        @Id override val commandId: String = IDProvider.next()
+    ) : TransactionCommand(), CreationCommand
+
+}
