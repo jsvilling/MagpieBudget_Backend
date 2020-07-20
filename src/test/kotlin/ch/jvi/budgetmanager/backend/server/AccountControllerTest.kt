8777d524fc54c196a51a6f07cd38b89496363e0b
Joshua Villing <joshua.villing@students.fhnw.ch>
2020-07-21 01:02:02 +0200

    Fix AccountControllerTest

--- src/test/kotlin/ch/jvi/budgetmanager/backend/server/AccountControllerTest.kt
+++ src/test/kotlin/ch/jvi/budgetmanager/backend/server/AccountControllerTest.kt
@@ -3,22 +3,29 @@ package ch.jvi.budgetmanager.backend.server
 import ch.jvi.budgetmanager.backend.api.command.store.CommandStore
 import ch.jvi.budgetmanager.backend.api.event.EventBus
 import ch.jvi.budgetmanager.backend.core.service.AccountService
+import ch.jvi.budgetmanager.backend.domain.account.AccountCommand
 import ch.jvi.budgetmanager.backend.server.controller.AccountController
 import org.junit.Test
 import org.mockito.Mockito.*
 import java.math.BigDecimal
+import java.math.BigDecimal.TEN
 
 internal class AccountControllerTest {
 
     val messageBus = mock(EventBus::class.java)
+
     val commandStore = mock(CommandStore::class.java)
+
     val accountService = spy(AccountService(messageBus, commandStore))
+
     val accountController = AccountController(accountService)
 
     @Test
     fun testGetAccount() {
         // Given
         val id = "someId"
+        val creationCommand = AccountCommand.CreateAccountCommand(TEN, "name", id)
+        `when`(commandStore.findCreationCommand(id)).thenReturn(creationCommand)
 
         // When
         accountController.get(id)
@@ -44,7 +51,7 @@ internal class AccountControllerTest {
     fun testUpdateAccount() {
         // Given
         val id = "id"
-        val balance = BigDecimal.TEN
+        val balance = TEN
         val name = "Name"
 
         // When
