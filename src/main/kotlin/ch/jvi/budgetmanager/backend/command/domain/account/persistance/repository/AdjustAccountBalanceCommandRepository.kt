b3a12f01090800a617362bead12794dc506c8b03
Joshua Villing <joshua.villing@students.fhnw.ch>
2020-08-22 18:49:39 +0200

    Fix wrong return type in AdjustAccountBalanceCommandRepository

--- src/main/kotlin/ch/jvi/budgetmanager/backend/command/domain/account/persistance/repository/AdjustAccountBalanceCommandRepository.kt
+++ src/main/kotlin/ch/jvi/budgetmanager/backend/command/domain/account/persistance/repository/AdjustAccountBalanceCommandRepository.kt
@@ -1,9 +1,8 @@
 package ch.jvi.budgetmanager.backend.command.domain.account.persistance.repository
 
-import ch.jvi.budgetmanager.backend.command.domain.account.command.AccountCommand
 import ch.jvi.budgetmanager.backend.command.domain.account.command.AccountCommand.AdjustAccountBalanceCommand
 import org.springframework.data.mongodb.repository.MongoRepository
 
 interface AdjustAccountBalanceCommandRepository : MongoRepository<AdjustAccountBalanceCommand, String> {
-    fun findByEntityId(entityId: String): List<AccountCommand.CreateAccountCommand>
+    fun findByEntityId(entityId: String): List<AdjustAccountBalanceCommand>
 }
